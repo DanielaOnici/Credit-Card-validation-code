23702e8b223b08b9c646070631f63a2fa13e96d8
Daniela Onici <danielaonici@gmail.com>
2022-03-31 15:01:22 -0400

    Verification code

---
+++ Credit card/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Credit_card
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            bool keepGoing = true;
+
+            do
+            {
+                try
+                {
+                    Console.Write("\nInput your full name: ");
+                    name = Console.ReadLine();
+
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.Write(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.Message);
+                }
+            } while (keepGoing);
+
+       
+
+        }
+    }
+}
